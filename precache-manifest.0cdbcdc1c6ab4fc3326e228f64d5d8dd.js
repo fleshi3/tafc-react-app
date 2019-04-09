9a86d1a0638f88d0d508bd295393616a9bd47719
fleshi3 <markuleh@gmail.com>
2019-04-09 19:24:37 +1000

    Updates

---
+++ precache-manifest.0cdbcdc1c6ab4fc3326e228f64d5d8dd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6e675bab75f5eb2258dd",
+    "url": "/tafc-react-app/static/css/main.af85668b.chunk.css"
+  },
+  {
+    "revision": "6e675bab75f5eb2258dd",
+    "url": "/tafc-react-app/static/js/main.fdde5a2d.chunk.js"
+  },
+  {
+    "revision": "b33a882c635f6d0d8d5c",
+    "url": "/tafc-react-app/static/js/runtime~main.0488088d.js"
+  },
+  {
+    "revision": "9fa25b87ea87c3e99e10",
+    "url": "/tafc-react-app/static/js/2.b78095b9.chunk.js"
+  },
+  {
+    "revision": "1e8d94510e655b8bca0287d0867cbf39",
+    "url": "/tafc-react-app/index.html"
+  }
+];
